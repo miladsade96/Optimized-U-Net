10f3596a47d74e0e876742da40b243faecf3b5b8
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-11-07 13:14:51 +0330

    Adding initial docstring

---
+++ model.py
@@ -0,0 +1,5 @@
+"""
+    Optimized U-Net for Brain Tumor Segmentation Model Implementation
+    Paper url: https://arxiv.org/pdf/2110.03352.pdf
+    Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
+"""
