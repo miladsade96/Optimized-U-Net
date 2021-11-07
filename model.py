f4d97d771076650af9112d5d79f91a91db3b62b0
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-11-07 15:41:30 +0330

    Optimizing import statements

--- model.py
+++ model.py
@@ -3,3 +3,8 @@
     Paper url: https://arxiv.org/pdf/2110.03352.pdf
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
 """
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.activations import sigmoid
+from tensorflow_addons.layers import InstanceNormalization
+from tensorflow.keras.layers import (Input, Conv3D, Conv3DTranspose, Concatenate, LeakyReLU, MaxPooling3D)
