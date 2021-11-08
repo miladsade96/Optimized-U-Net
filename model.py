7689610f2ce866a7fddcd83bfcbce8bb94a0dc4d
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-11-08 17:31:38 +0330

    Fixing encoder private function

--- model.py
+++ model.py
@@ -10,17 +10,24 @@ from tensorflow_addons.layers import InstanceNormalization
 from tensorflow.keras.layers import (Conv3D, Conv3DTranspose, Concatenate, LeakyReLU, MaxPooling3D, Input)
 
 
-def _encoder(pl, num_fil):
+def _encoder(pl, num_fil, is_first_conv=False):
     """
     Encoder private function that only applies Conv3D, Instance Normalization and LeakyReLU
     :param pl: previous layer
     :param num_fil: number of filters in Conv3D layer
+    :param is_first_conv: whether this conv block is the first one or not
     :return: tensor
     """
-    c_1 = Conv3D(filters=num_fil, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding="same")(pl)
+    fcls = (2, 2, 2)    # first convolutional layer strides
+    scls = (1, 1, 1)    # second convolutional layer strides
+    # first conv block condition
+    if is_first_conv:
+        fcls = (1, 1, 1)
+
+    c_1 = Conv3D(filters=num_fil, kernel_size=(3, 3, 3), strides=fcls, padding="same")(pl)
     in_1 = InstanceNormalization()(c_1)
     lr_1 = LeakyReLU(alpha=0.01)(in_1)  # Activation layer
-    c_2 = Conv3D(filters=num_fil, kernel_size=(3, 3, 3), strides=(1, 1, 1), padding="same")(lr_1)
+    c_2 = Conv3D(filters=num_fil, kernel_size=(3, 3, 3), strides=scls, padding="same")(lr_1)
     in_2 = InstanceNormalization()(c_2)
     lr_2 = LeakyReLU(alpha=0.01)(in_2)  # Activation layer
     return lr_2
