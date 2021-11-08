6278f8525ff3202994fbeff9cb31d49b8c4c861e
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-11-08 16:36:24 +0330

    Importing tf Model

--- model.py
+++ model.py
@@ -4,6 +4,7 @@
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
 """
 
+from tensorflow.keras.models import Model
 from tensorflow.keras.activations import sigmoid
 from tensorflow_addons.layers import InstanceNormalization
 from tensorflow.keras.layers import (Conv3D, Conv3DTranspose, Concatenate, LeakyReLU, MaxPooling3D, Input)
